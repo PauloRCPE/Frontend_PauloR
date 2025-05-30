c3f751a3e02d00447d09262d5047d8527638d74f
Paulo R <pauloorbolato@cpejr.com.br>
2025-05-30 15:09:56 -0300

    feat: Expanded front

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import "../src/App.css";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+import { DivTeste2 } from "./AppStyles";
+import { useState, useEffect, use } from "react";
+
 function App() {
-  return <div>oi otario</div>;
+  const [contador, setContador] = useState(0);
+  contador = 0;
+
+  useEffect(() => {
+    alert("abriu");
+  }, [contador == 100]);
+
+  return (
+    <div>
+      <Button
+        onClick={() => setContador(contador + 1)}
+        type="primary"
+        size="large"
+        icon={<DownloadOutlined />}
+      >
+        {contador}
+      </Button>
+      <DivTeste2>ola essa porra tá aq?</DivTeste2>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/AppStyles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DivTeste2 = styled.div`
+  width: 50%;
+  height: 25%;
+  background-color: blue;
+`;
